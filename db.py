f6f957f37f70474141c3baa128e86cb11a02be40
PolinaK <polina.kirichencko@yandex.ru>
2023-08-26 20:47:45 +0400

    database tables and connection to them written

---
+++ db.py
@@ -0,0 +1,49 @@
+import sqlite3
+import click
+from flask import current_app, g
+
+
+def connect_db():
+    """Connection to database"""
+    conn = sqlite3.connect(current_app.config['DATABASE'], detect_types=sqlite3.PARSE_DECLTYPES)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def get_db():
+    """Connect to the application's configured database. The connection
+       is unique for each request and will be reused if this is called
+       again.
+       """
+    if not hasattr(g, 'db'):
+        g.db = connect_db()
+    return g.db
+
+
+def create_db():
+    """Creating a database"""
+    db = get_db()
+    with current_app.open_resource('schema.sql', mode='r') as f:
+        db.executescript(f.read().decode('utf8'))
+
+
+def close_db(error=None):
+    """If this request connected to the database, close the
+            connection.
+            """
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
+
+
+@click.command('init-db')
+def init_db_command():
+    """Deleting existing data and creating tables (with the message of success)"""
+    create_db()
+    click.echo('Создание базы данных')
+
+
+def init_app(app):
+    """Takes in the app and registrates it"""
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
---
+++ schema.sql
@@ -0,0 +1,37 @@
+CREATE TABLE IF NOT EXISTS restaurants(
+id integer PRIMARY KEY AUTOINCREMENT,
+title text NOT NULL,
+url text NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS AUmenu(
+id integer PRIMARY KEY AUTOINCREMENT,
+name text NOT NULL,
+url text NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS NAUmenu(
+id integer PRIMARY KEY AUTOINCREMENT,
+name text NOT NULL,
+url text NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS feedbacks(
+id integer PRIMARY KEY AUTOINCREMENT,
+author_id integer NOT NULL,
+title text NOT NULL,
+body text NOT NULL,
+url text NOT NULL,
+created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+FOREIGN KEY (author_id) REFERENCES users (id)
+);
+
+CREATE TABLE IF NOT EXISTS users(
+id integer PRIMARY KEY AUTOINCREMENT,
+username text NOT NULL,
+email text NOT NULL,
+password text NOT NULL,
+avatar BLOB DEFAULT NULL,
+);
+
