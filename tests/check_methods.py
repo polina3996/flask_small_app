c543e7f04e28ab7921c88efcd1f4c8798cff52e3
PolinaK <polina.kirichencko@yandex.ru>
2023-09-16 22:06:22 +0400

    changes in the names of test

--- tests/check_methods.py
+++ tests/check_methods.py
@@ -1,33 +1,33 @@
 from flask import make_response
 
 
-def check_status_code_200(client, url):
+def check_status_code_200_get(client, url):
     response = client.get(url)
     assert response.status_code == 200
 
 
-def check_status_code_302(client, url):
+def check_status_code_302_get(client, url):
     response = client.get(url)
     assert response.status_code == 302
 
 
-def check_content_text_type(client, url):
+def check_content_text_type_get(client, url):
     response = client.get(url)
     assert response.content_type == 'text/html; charset=utf-8'
 
 
-def check_content_image_type(client, url):
+def check_content_image_type_get(client, url):
     response = client.get(url)
     assert response.content_type == 'image/png'
 
 
-def check_404(client, url):
+def check_404_get(client, url):
     response = client.get(url)
     assert b'html' in response.data
     assert b'p class="none"' in response.data
 
 
-def check_login_required(app, client, url):
+def check_login_required_get(app, client, url):
     # creating the app context
     with app.app_context():
         response = client.get(url)
@@ -37,26 +37,26 @@ def check_login_required(app, client, url):
         assert '/login' in resp.headers["Location"]
 
 
-def check_nonauth_menu(client, url):
+def check_nonauth_menu_get(client, url):
     response = client.get(url)
     assert b'a href="/index"' in response.data
     assert b'a href="/login"' in response.data
     assert b'a href="/register"' in response.data
 
 
-def check_auth_menu(client, url):
+def check_auth_menu_get(client, url):
     response = client.get(url)
     assert b'a href="/index"' in response.data
     assert b'li class="username"' in response.data
     assert b'a href="/logout"' in response.data
 
 
-def check_index_data(client, url='/index'):
+def check_index_data_get(client, url='/index'):
     response = client.get(url)
     assert b'table class="review"' in response.data
 
 
-def check_rest_data(client, url):
+def check_rest_data_get(client, url):
     response = client.get(url)
     assert b'html' in response.data
     assert b'ul class="mainmenu"' in response.data
@@ -66,13 +66,13 @@ def check_rest_data(client, url):
     assert b'p class="contacts"' in response.data
 
 
-def check_nonexist_rest(client, url):
-    check_404(client, url)
+def check_nonexist_rest_get(client, url):
+    check_404_get(client, url)
 
 
-def check_profile_auth_data(client, url):
+def check_profile_auth_data_get(client, url):
     response = client.get(url)
-    check_auth_menu(client, url)
+    check_auth_menu_get(client, url)
     assert b'html' in response.data
     assert b'table class="profile-table"' in response.data
     assert b'div class ="profile-ava"' in response.data
--- tests/test_app.py
+++ tests/test_app.py
@@ -3,11 +3,9 @@ import sys
 import pytest
 from flask import make_response
 from app import create_app
-from check_methods import check_status_code_200, check_status_code_302, check_content_text_type, \
-    check_content_image_type, check_nonauth_menu, \
-    check_auth_menu, check_index_data, \
-    check_rest_data, check_nonexist_rest, \
-    check_profile_auth_data, check_login_required
+from check_methods import check_status_code_200_get, check_content_text_type_get, check_nonauth_menu_get, \
+    check_index_data_get, check_rest_data_get, check_nonexist_rest_get, check_status_code_302_get, \
+    check_login_required_get
 
 
 def test_config():
@@ -21,41 +19,41 @@ def test_config():
 def test_index(client, auth):
     """Test of main page: the differences between logged and non-logged in are only in mainmemu"""
     # non logged in
-    check_status_code_200(client, '/index')
-    check_content_text_type(client, '/index')
-    check_nonauth_menu(client, '/index')
-    check_index_data(client)
+    check_status_code_200_get(client, '/index')
+    check_content_text_type_get(client, '/index')
+    check_nonauth_menu_get(client, '/index')
+    check_index_data_get(client)
 
     # logged in
     # auth.login()
-    # check_status_code_200(client, '/index')
-    # check_content_text_type(client, '/index')
-    # check_auth_menu(client, '/index')
-    # check_index_data(client)
+    # check_status_code_200_get(client, '/index')
+    # check_content_text_type_get(client, '/index')
+    # check_auth_menu_get(client, '/index')
+    # check_index_data_get(client)
 
 
 @pytest.mark.parametrize('rest_id', [1, 2, 3, 4, 5, 6])
 def test_restaurant(client, rest_id):
     """Test of any of restaurant pages: the differences between logged and non-logged in are only in mainmemu"""
     # non logged in
-    check_status_code_200(client, f'/restaurant/{rest_id}')
-    check_content_text_type(client, f'/restaurant/{rest_id}')
-    check_nonauth_menu(client, f'/restaurant/{rest_id}')
-    check_rest_data(client, f'/restaurant/{rest_id}')
+    check_status_code_200_get(client, f'/restaurant/{rest_id}')
+    check_content_text_type_get(client, f'/restaurant/{rest_id}')
+    check_nonauth_menu_get(client, f'/restaurant/{rest_id}')
+    check_rest_data_get(client, f'/restaurant/{rest_id}')
 
     # logged in
     # auth.login()
-    # check_status_code_200(client, f'/restaurant/{rest_id}')
-    # check_content_text_type(client, f'/restaurant/{rest_id}')
-    # check_auth_menu(client, f'/restaurant/{rest_id}')
-    # check_rest_data(client, f'/restaurant/{rest_id}')
+    # check_status_code_200_get(client, f'/restaurant/{rest_id}')
+    # check_content_text_type_get(client, f'/restaurant/{rest_id}')
+    # check_auth_menu_get(client, f'/restaurant/{rest_id}')
+    # check_rest_data_get(client, f'/restaurant/{rest_id}')
 
 
 def test_restaurant_nonexists(client, rest_id=sys.maxsize):
     """Test of the page when restaurant doesn't exist: returns 404 page not found"""
-    check_status_code_200(client, f'/restaurant/{rest_id}')
-    check_content_text_type(client, f'/restaurant/{rest_id}')
-    check_nonexist_rest(client, f'/restaurant/{rest_id}')
+    check_status_code_200_get(client, f'/restaurant/{rest_id}')
+    check_content_text_type_get(client, f'/restaurant/{rest_id}')
+    check_nonexist_rest_get(client, f'/restaurant/{rest_id}')
 
 
 @pytest.mark.parametrize('username', ['test_name', 'test_username'])
@@ -63,41 +61,41 @@ def test_profile(app, client, username):
     """Test of the profile page: the differences between logged and non-logged in are redirection to login to see the
     PROFILE page """
     # non logged in
-    check_status_code_302(client, f'/profile/{username}')
-    check_content_text_type(client, f'/profile/{username}')
-    check_login_required(app, client, f'/profile/{username}')
+    check_status_code_302_get(client, f'/profile/{username}')
+    check_content_text_type_get(client, f'/profile/{username}')
+    check_login_required_get(app, client, f'/profile/{username}')
 
     # logged in
-    # check_status_code_200(client, f'/profile/{username}')
-    # check_content_text_type(client, f'/profile/{username}')
-    # check_profile_auth_data(client, f'/profile/{username}')
+    # check_status_code_200_get(client, f'/profile/{username}')
+    # check_content_text_type_get(client, f'/profile/{username}')
+    # check_profile_auth_data_get(client, f'/profile/{username}')
 
-    
+
 @pytest.mark.parametrize('username', ['test_name', 'test_username'])
 def test_userava_exists(app, client, username):
     # non logged in
-    check_status_code_302(client, f'/profile/{username}')
-    check_content_text_type(client, f'/profile/{username}')
-    check_login_required(app, client, f'/profile/{username}')
+    check_status_code_302_get(client, f'/profile/{username}')
+    check_content_text_type_get(client, f'/profile/{username}')
+    check_login_required_get(app, client, f'/profile/{username}')
 
     # logged in
-    # check_status_code_200(client, f'/userava/{username}')
-    # check_content_image_type(client, f'/userava/{username}')
+    # check_status_code_200_get(client, f'/userava/{username}')
+    # check_content_image_type_get(client, f'/userava/{username}')
 
 
 def test_profile_and_userava_nonexists(app, client, username=random.random()):
     """Test of the profile and userava pages that don't exist: the differences between logged and non-logged in are redirection to
     login to see the INDEX page """
     # non logged in
-    check_status_code_302(client, f'/profile/{username}')
-    check_content_text_type(client, f'/profile/{username}')
-    check_login_required(app, client, f'/profile/{username}')
+    check_status_code_302_get(client, f'/profile/{username}')
+    check_content_text_type_get(client, f'/profile/{username}')
+    check_login_required_get(app, client, f'/profile/{username}')
 
     # logged in
-    # check_status_code_200(client, f'/profile/{username}')
-    # check_content_text_type(client, f'/profile/{username}')
-    # check_auth_menu(client, '/index')
-    # check_index_data(client)
+    # check_status_code_200_get(client, f'/profile/{username}')
+    # check_content_text_type_get(client, f'/profile/{username}')
+    # check_auth_menu_get(client, '/index')
+    # check_index_data_get(client)
 
 
 
